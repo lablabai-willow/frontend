8fac4606dffbe6fe84042fa198916f6b1c6f0ad9
AliMustafa253 <alimustafa2599@gmail.com>
2023-12-23 23:08:41 +0500

    updated chat window

--- src/components/ChatWindow/ChatWindow.jsx
+++ src/components/ChatWindow/ChatWindow.jsx
@@ -1,41 +1,60 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useSelector } from 'react-redux';
-import typingDots from '../../assets/typing2.svg'
-import { MessageBubble } from '../../components';
-import './ChatWindow.css';
-import { LOADING_STATES } from '../../constants';
+import React, { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import typingDots from "../../assets/typing2.svg";
+import { MessageBubble } from "../../components";
+import willowProfilePic from "../../assets/willow.png";
+import "./ChatWindow.css";
+import { LOADING_STATES } from "../../constants";
 
 const ChatWindow = ({ convoHistory }) => {
-    const scrollRef = useRef(null);
-    const typingBubbleVisible = useSelector((state) => {
-        return state.app.loading === LOADING_STATES.PENDING && state.message.tempMessage !== null;
-    })
-    const tempMessage = useSelector((state) => {
-        return state.message.tempMessage
-    })
-
-    useEffect(() => {
-        if (scrollRef.current) {
-            scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
-        }
-    }, [convoHistory]) ;
-
-    
+  const scrollRef = useRef(null);
+  const chatboxRef = useRef(null);
+  const typingBubbleVisible = useSelector((state) => {
     return (
-        <div ref={scrollRef} className="chat-window">
-            {convoHistory.map(( messageObject ) => {
-                return <MessageBubble refToPass={scrollRef} key={messageObject.key} {...messageObject} />
-            })}
-            {tempMessage && <MessageBubble refToPass={scrollRef} key="TEMP_OBJECT" {...tempMessage} />}
-            {typingBubbleVisible && (
-                <div ref={scrollRef} className="typing-icon-container">
-                    <div className="typing-icon-bubble ">
-                        <img src={typingDots} className="typing-icon" />
-                    </div>
-                </div>
-            )}
-        </div>    
-    )
-}
+      state.app.loading === LOADING_STATES.PENDING &&
+      state.message.tempMessage !== null
+    );
+  });
+  const tempMessage = useSelector((state) => {
+    return state.message.tempMessage;
+  });
+
+  const scrollToBottom = () => {
+    chatboxRef.current.scrollTop = chatboxRef.current.scrollHeight;
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [convoHistory]);
+
+  return (
+    <div ref={chatboxRef} className="chat-window">
+      {convoHistory.length === 0 ? (
+        <div className="intro-container">
+          <img
+            className="intro-picture"
+            src={willowProfilePic}
+            alt="willow-picture"
+          />
+          <h2 className="intro-text">
+            Hi, I'm Willow. I'm here for you. What's on your mind today?
+          </h2>
+        </div>
+      ) : (
+        convoHistory.map((messageObject) => {
+          return <MessageBubble key={messageObject.key} {...messageObject} />;
+        })
+      )}
+      {tempMessage && <MessageBubble key="TEMP_OBJECT" {...tempMessage} />}
+      {typingBubbleVisible && (
+        <div className="typing-icon-container">
+          <div className="typing-icon-bubble ">
+            <img src={typingDots} className="typing-icon" />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default ChatWindow;
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,33 +1,44 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { clearConversationHistory } from '../../store/messageSlice';
-import './Menu.css'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { clearConversationHistory } from "../../store/messageSlice";
+import "./Menu.css";
 
-const Menu = ({ visible=false, handleFileUpload }) => {
-    const dispatch = useDispatch()
-    const navigate = useNavigate();
-    const env = useSelector((state) => {
-        return state.app.env;
-    });
+const Menu = ({ visible = false, handleFileUpload }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const env = useSelector((state) => {
+    return state.app.env;
+  });
 
-    const handleClearConversationHistory = () => {
-        dispatch(clearConversationHistory(env))
-    }
+  const handleClearConversationHistory = () => {
+    dispatch(clearConversationHistory(env));
+  };
 
-    const navigateToDraw = () => {
-        navigate('/Draw');
-    }
+  const navigateToDraw = () => {
+    navigate("/Draw");
+  };
 
-    return (
-        <div className={`menu-container ${!visible ? "hidden" : ""}`}>
-            <button onClick={handleFileUpload} className="menu-item">Send an Image</button>
-            <span className="divider"/>
-            <button onClick={navigateToDraw} className="menu-item">Draw a Picture</button>
-            <span className="divider"/>
-            <button onClick={handleClearConversationHistory} className="menu-item clear-chat" >Clear Chat</button>
-        </div>
-    )
+  return (
+    <div className="menu-container">
+      <div className={`menu-window ${!visible ? "hidden" : ""}`}>
+        <button onClick={handleFileUpload} className="menu-item">
+          Send an Image
+        </button>
+        <span className="divider" />
+        <button onClick={navigateToDraw} className="menu-item">
+          Draw a Picture
+        </button>
+        <span className="divider" />
+        <button
+          onClick={handleClearConversationHistory}
+          className="menu-item clear-chat"
+        >
+          Clear Chat
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
